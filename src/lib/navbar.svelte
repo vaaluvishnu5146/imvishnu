9e377c40eeee199a8bee27f775ea9c95e5b2c9d7
ananduremanan <ananduor@gmail.com>
2024-03-15 10:55:54 +0530

    WebPage Updation

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,6 @@
+<nav class="sticky top-0 p-4 border-y-black border bg-yellow-400 flex flex-row justify-between lg:px-28">
+	<div>
+        <img src="/images/port_icon.png" alt="icon" class="w-8">
+    </div>
+	<button class="bg-black text-white p-2 rounded-full text-sm">Know More</button>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Navbar from '$lib/navbar.svelte';
+	import '../app.css';
+</script>
+
+<Navbar />
+<main>
+	<section
+		class="h-screen bg-yellow-400 px-4 flex flex-col justify-center items-start lg:px-24 lg:grid lg:grid-cols-2"
+	>
+		<div class="lg:flex lg:flex-col lg:items-start lg:h-full lg:justify-center justify-center">
+			<div class="font-kalnia text-4xl font-medium lg:text-6xl">Your Neighbourhood Developer.</div>
+			<div class="font-light mt-4">Full Stack Developer who loves to build hybrid applications</div>
+			<button class="bg-black text-white p-2 rounded-full mt-4 text-sm">Download Resume</button>
+		</div>
+		<div></div>
+	</section>
+</main>
